bf25099c196629fcf13d2bfc953bd83dd227f075
Kristian Morath <kristian.morath2@bluewin.ch>
2018-12-27 20:35:54 +0100

    Version_002.3

--- _testing/main.cpp
+++ _testing/main.cpp
@@ -6,6 +6,8 @@ int main()
 	std::cout << "Hallo" << std::endl;
 	std::cout << "Bug-FIX" << std::endl;
 	std::cout << "Bug-FIX2" << std::endl;
+	std::cout << "Bug-FIX3" << std::endl;
+
 
 	system("PAUSE");
 	return 0;
