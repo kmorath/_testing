76307d68bf31a146560c6ad04fd63901750dc87d
Kristian Morath <kristian.morath2@bluewin.ch>
2018-12-27 21:12:45 +0100

    Fihnal Release

--- _testing/main.cpp
+++ _testing/main.cpp
@@ -4,18 +4,16 @@ int main()
 {
 	std::cout << "Hallo" << std::endl;
 	std::cout << "Hallo" << std::endl;
-<<<<<<< HEAD
 	std::cout << "Bug-FIX" << std::endl;
 	std::cout << "Bug-FIX2" << std::endl;
 	std::cout << "Bug-FIX3" << std::endl;
 	std::cout << "RELEASE" << std::endl;
-=======
 	std::cout << "Hallo" << std::endl;
 	std::cout << "Hallo" << std::endl;
 	std::cout << "Hallo" << std::endl;
 	std::cout << "Hallo" << std::endl;
 	std::cout << "Neu" << std::endl;
->>>>>>> master
+
 
 
 	system("PAUSE");
