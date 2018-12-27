269b7e1f219fbd658a0b3eef5231ec336abe79c1
Kristian Morath <kristian.morath2@bluewin.ch>
2018-12-27 20:19:05 +0100

    Version8

--- _testing/main.cpp
+++ _testing/main.cpp
@@ -8,6 +8,8 @@ int main()
 	std::cout << "Hallo" << std::endl;
 	std::cout << "Hallo" << std::endl;
 	std::cout << "Hallo" << std::endl;
+	std::cout << "Neu" << std::endl;
+
 
 	system("PAUSE");
 	return 0;
